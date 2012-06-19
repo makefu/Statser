8ec86826c54d4343f1c03b10809fe5458f81e74c
Felix Richter <Felix.Richter@alcatel-lucent.com>
2012-06-19 12:48:17 +0200

    add test cases for Windows which do not fail

--- test/testPsutilStatser.py
+++ test/testPsutilStatser.py
@@ -1,33 +1,44 @@
 import unittest
 import sys
+import platform
 sys.path.append("..")
 
 from Statser import Statser
 
-class TestBasicStatser(unittest.TestCase):
+class TestPsutilStatser(unittest.TestCase):
     def test_collect_disk(self):
         s = Statser()
         s.collect_disk_io()
         self.assertTrue(s.db)
     def test_collect_disk_whitelist(self):
         s = Statser()
-        s.collect_disk_io(["sda1"])
+        if platform.system() == "Windows":
+          d = "PhysicalDrive0"
+        else:
+          d = "sda1"
+        s.collect_disk_io([d])
         self.assertTrue(s.db)
         for e in s.db:
             # make sure that every entry has only sda1 in the name
-            self.assertTrue("sda1" in e["name"])
+            self.assertTrue(d in e["name"])
 
     def test_collect_network(self):
         s = Statser()
         s.collect_network_io()
         self.assertTrue(s.db)
 
-    def test_collect_disk_whitelist(self):
+    def test_collect_network_whitelist(self):
         s = Statser()
-        s.collect_network_io(["lo"])
+        needle="lo"
+        if platform.system() == "Windows":
+          lo="MS TCP Loopback interface"
+          needle=lo.replace(" ","_")
+        else:
+          lo="lo"
+        s.collect_network_io([lo])
         self.assertTrue(s.db)
         for e in s.db:
-            self.assertTrue("lo" in e["name"])
+            self.assertTrue(needle in e["name"])
 
     def test_collect_disk_usage(self):
         s = Statser()
@@ -40,10 +51,16 @@ class TestBasicStatser(unittest.TestCase):
         test both, translation of the name / to root and the whitelist
         """
         s = Statser()
-        s.collect_disk_usage("/")
+        needle="root"
+        if platform.system() == "Windows":
+          s.collect_disk_usage("C:\\\\")
+          needle="C"
+        else:
+          s.collect_disk_usage("/")
+
         self.assertTrue(s.db)
         for e in s.db:
-            self.assertTrue("root" in e["name"])
+            self.assertTrue(needle in e["name"])
 
 if __name__ == '__main__':
         unittest.main()
